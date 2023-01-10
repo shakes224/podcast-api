2eceaf6cba374bed0d73ba47b1b5b6b02308be95
Keketso Leu <keketsoleu12@gmail.com>
2023-01-10 08:47:48 +0200

    started again

--- scripts.js
+++
@@ -1,6 +0,0 @@
-import  './components/podcast-app.js'
-import  './components/podcast-episode.js'
-import  './components/podcast-meta.js'
-import  './components/podcast-preview.js'
-import  './components/podcast-view-list.js'
-import  './components/podcast-view-single.js'
